8951878cc85ef5b5e6eb72117a1e7e9bff094d1b
FranJ2000 <45925901+FranJ2000@users.noreply.github.com>
2019-11-19 16:05:50 +0100

    Primera version

---
+++ web1/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="vendor/fontawesome-5.11.2/css/all.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="">
+    
+    <title>Inicio | Taller</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="http://localhost/web1/index.php">Taller</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto text-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="http://localhost/web1/index.php">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://localhost/web1/login.php">Iniciar Sesión</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <?php
+      require_once 'footer.php';
+    ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="vendor/jquery-3.3.1.slim.min.js"></script>
+    <script src="vendor/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
