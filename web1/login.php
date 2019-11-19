23b7a1cb6435fc66272b13a2fc5b7f49233f2733
FranJ2000 <45925901+FranJ2000@users.noreply.github.com>
2019-11-19 20:35:22 +0100

    LoginV,1 Casi terminado
    Implementa lo que falta para sesiones.

--- web1/login.php
+++ web1/login.php
@@ -36,22 +36,68 @@
     </header>
     
     <div class="container mt-2 mb-2 bg-white">
-        <div class="container row border-info pb-3 pt-3">
-            <div class="col-12 col-md-6 offset-md-3 border border-success rounded bg-light">
-                <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="">
-                    <div class="form-group mb-2">
-                        <h1 class="text-center">Iniciar Sesión</h1>
-                        <label for="nombre">Usuario</label> 
-                        <input type='text' name='usuario' id="usuario" placeholder="" class="form-control" required>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="dni">Contraseña</label>
-                        <input type='password' class="form-control" name='password' id="password" required>
-                    </div>
-                    <p> <input type="submit" class="btn btn-primary w-100" name='submit' value="Iniciar Sesión"></p>
-                </form>
+      <div class="container row border-info pb-3 pt-3">
+        <div class="col-12 col-md-6 offset-md-3 border border-success rounded bg-light">
+          <?php
+            require_once 'Conn.php';
+            
+            class Login extends Conn {
+              public function __construct() {
+                  parent::__construct();
+              }
+
+              public function load($user,$password) {
+                $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+                $login = $this->connection->prepare('SELECT * FROM usuarios WHERE user = ? AND password = ?');
+                $login->bindParam('1',$user);
+                $login->bindParam('2',$hash);
+                $login->execute();
+                $linea = $login->fetch();
+
+                if($login->fetch()==0) {
+                  switch ($linea['tipo']) {
+                    case 'Cliente':
+                      header("Location:cliente.php");
+                      break;
+                    case 'Empleado':
+                      header("Location:empleado.php");
+                      break;
+                    case 'Admin':
+                      header("Location:admin.php");
+                      break;
+                    default:
+                      header("Location:index.php");
+                      break;
+                  }
+                } else {
+                  return '<div class="alert alert-danger mt-2" role="alert">
+                        ¡Datos incorrectos!
+                    </div>';
+                }
+              }
+            }
+          ?>
+          <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="">
+            <div class="form-group mb-2">
+              <h1 class="text-center">Iniciar Sesión</h1>
+              <label for="usuario">Usuario</label> 
+              <input type='text' name='usuario' id="usuario" placeholder="" class="form-control" required>
             </div>
+            <div class="form-group mb-2">
+              <label for="password">Contraseña</label>
+              <input type='password' class="form-control" name='password' id="password" required>
+            </div>
+            <p> <input type="submit" class="btn btn-primary w-100" name='submit' value="Iniciar Sesión"></p>
+          </form>
+          <?php
+            if(isset($_POST['submit'])) {
+              $log = new Login();
+              echo $log->load($_POST["usuario"],$_POST["password"]);
+            }
+          ?>
         </div>
+      </div>
     </div>
 
     <?php
