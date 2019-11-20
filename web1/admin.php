05101c8157de58be3faeb0f927201aae562941c8
FranJ2000 <45925901+FranJ2000@users.noreply.github.com>
2019-11-20 17:18:43 +0100

    Implantado sesiones.
    Va en la raíz del proyecto.

--- web1/admin.php
+++ web1/admin.php
@@ -1,3 +1,6 @@
+<?php 
+  session_start(); 
+?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -15,57 +18,15 @@
     <title>Panel | Admin </title>
   </head>
   <body>
-    <header>
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="http://localhost/web1/admin/admin.php">Admin Área</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav mr-auto text-center">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarCliente" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Cliente
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarCliente">
-                <a class="dropdown-item" href="#">Ver</a>
-                <a class="dropdown-item" href="#">Borrar</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarEmpleado" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Empleado
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarEmpleado">
-                <a class="dropdown-item" href="#">Agregar</a>
-                <a class="dropdown-item" href="#">Ver</a>
-                <a class="dropdown-item" href="#">Borrar</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarVehiculo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Vehículo
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarVehiculo">
-                <a class="dropdown-item" href="#">Ver</a>
-                <a class="dropdown-item" href="#">Modificar</a>
-                <a class="dropdown-item" href="#">Borrar</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://localhost/web1/logout.php">Cerrar Sesión</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    <?php 
+      require_once 'menuAdmin.php';
+    ?>
     <body>
       <div class="container-fluid">
         <div class="row">
           <div class="col-12">
             <div class="alert alert-primary m-2" role="alert">
-              Bienvenido, <?php //Nombre usuario ?>!
+              Bienvenido, <?php echo $_SESSION['cliente'] ?>!
             </div>
           </div>
         </div>
---
+++ web1/menuAdmin.php
@@ -0,0 +1,45 @@
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="http://localhost/web1/admin/admin.php">Admin Área</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav mr-auto text-center">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarCliente" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Cliente
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarCliente">
+                <a class="dropdown-item" href="#">Ver</a>
+                <a class="dropdown-item" href="#">Borrar</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarEmpleado" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Empleado
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarEmpleado">
+                <a class="dropdown-item" href="#">Agregar</a>
+                <a class="dropdown-item" href="#">Ver</a>
+                <a class="dropdown-item" href="#">Borrar</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarVehiculo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Vehículo
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarVehiculo">
+                <a class="dropdown-item" href="#">Ver</a>
+                <a class="dropdown-item" href="#">Modificar</a>
+                <a class="dropdown-item" href="#">Borrar</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://localhost/web1/logout.php">Cerrar Sesión</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
