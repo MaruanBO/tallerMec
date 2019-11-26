821c43ac1155439f53d80b5cca702a318ad84834
MaruanBO <maruanbueno55@gmail.com>
2019-11-26 09:29:35 +0100

    Final Versión

---
+++ cliente/class/Conn.php
@@ -0,0 +1,13 @@
+<?php
+        class Conn {
+
+            public function __construct() {
+                try {
+                    $this->connection = new PDO("mysql:host=localhost;dbname=tallerMec", 'root', 'root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));  
+                }
+                catch (PDOException $e) {
+                    echo 'Falló la conexión: ' . $e->getMessage();
+                } 
+            }
+        }
+?>
